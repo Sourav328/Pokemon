942fde4e899ae445d09c232a76bebcdf1380a5aa
Sourav <souravdhali126@gmail.com>
2024-10-06 01:07:27 +0530

    Trying to add Multiple file at ones

---
+++ ChoosePk.cpp
@@ -0,0 +1,14 @@
+
+
+void ChoosePokemon() 
+{
+	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+	cout << "You can choose one of the following Pokémon:\n";
+	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+	cout << "Which Pokémon would you like to choose? Enter the number: ";
+
+	int choice;
+	cin >> choice;
+
+	return 0;
+}
