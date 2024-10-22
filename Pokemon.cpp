cc8cf169f8dd53d12c7a29c2abe7c1fd56cd0878
Sourav <souravdhali126@gmail.com>
2024-10-23 00:10:35 +0530

    Creating Classes and Objects

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -1,48 +1,47 @@
-<<<<<<< Updated upstream
 #include <iostream>
 #include<string>
 using namespace std;
 
-void Game_Start()
-{
-    cout << "Ah, Trainer!" << endl;
-    cout << "Welcome to the world of Pokemon! I am Professor Oak." << endl;
-    cout << "Today is a momentous day—you will be choosing your very first Pokemon." << endl;
-    cout << "Every great Trainer remembers this moment for the rest of their lives." << endl;
-    cout << "So, choose wisely, young one!" << endl;
-    cout << "1. Bulbasaur" << endl;
-    cout << "2. Charmander " << endl;
-    cout << "3. Squirtle " << endl;
-}
-
-void Game_End()
-{
-    cout << "Professor Oak: Ah, an excellent choice!" << endl;
-    cout << "Professor Oak: But beware, Trainer," << endl;
-    cout << "Professor Oak: This is only the beginning. Your journey is about to unfold." << endl;
-    cout << "Professor Oak: Now let's see if you've got what it takes to keep going!" << endl;
-    cout << "Professor Oak: Good luck, and remember—choose wisely!" << endl;
-}
-
-void Professor_Oak()
-{
-    cout << "                        " << endl;
-    cout << "Professor Oak: Ah, young Trainer!" << endl;
-    cout << "Professor Oak: I see you've made a great start," << endl;
-    cout << "Professor Oak: but do you really think we're done yet?" << endl;
-    cout << "Professor Oak: No, no, no!" << endl;
-    cout << "Professor Oak: We've only just scratched the surface!" << endl;
-}
-
-void Tracey()
-{
-    cout << "                        " << endl;
-    cout << "Tracey: So, you have been following Oak’s every word, huh?" << endl;
-    cout << "Tracey: Cute." << endl;
-    cout << "Tracey: But the real battles require more than just basic programming." << endl;
-    cout << "Tracey: Today, you're going to clean up some mess in your code." << endl;
-    cout << "Tracey: Let's see if you can level up your enums... if you’r ready, of course." << endl;
-}
+//void Game_Start()
+//{
+//    cout << "Ah, Trainer!" << endl;
+//    cout << "Welcome to the world of Pokemon! I am Professor Oak." << endl;
+//    cout << "Today is a momentous day—you will be choosing your very first Pokemon." << endl;
+//    cout << "Every great Trainer remembers this moment for the rest of their lives." << endl;
+//    cout << "So, choose wisely, young one!" << endl;
+//    cout << "1. Bulbasaur" << endl;
+//    cout << "2. Charmander " << endl;
+//    cout << "3. Squirtle " << endl;
+//}
+//
+//void Game_End()
+//{
+//    cout << "Professor Oak: Ah, an excellent choice!" << endl;
+//    cout << "Professor Oak: But beware, Trainer," << endl;
+//    cout << "Professor Oak: This is only the beginning. Your journey is about to unfold." << endl;
+//    cout << "Professor Oak: Now let's see if you've got what it takes to keep going!" << endl;
+//    cout << "Professor Oak: Good luck, and remember—choose wisely!" << endl;
+//}
+//
+//void Professor_Oak()
+//{
+//    cout << "                        " << endl;
+//    cout << "Professor Oak: Ah, young Trainer!" << endl;
+//    cout << "Professor Oak: I see you've made a great start," << endl;
+//    cout << "Professor Oak: but do you really think we're done yet?" << endl;
+//    cout << "Professor Oak: No, no, no!" << endl;
+//    cout << "Professor Oak: We've only just scratched the surface!" << endl;
+//}
+//
+//void Tracey()
+//{
+//    cout << "                        " << endl;
+//    cout << "Tracey: So, you have been following Oak’s every word, huh?" << endl;
+//    cout << "Tracey: Cute." << endl;
+//    cout << "Tracey: But the real battles require more than just basic programming." << endl;
+//    cout << "Tracey: Today, you're going to clean up some mess in your code." << endl;
+//    cout << "Tracey: Let's see if you can level up your enums... if you’r ready, of course." << endl;
+//}
 
 
 enum class PokemonChoice {
@@ -142,4 +141,3 @@ int main()
 
     return 0;
 }
->>>>>>> Stashed changes
