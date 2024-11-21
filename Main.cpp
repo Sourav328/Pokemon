f364a9af3ba67780efd18c36a5b33b0bfda93b32
Sourav <souravdhali126@gmail.com>
2024-11-22 01:38:18 +0530

    Set the stage for combat

--- Main.cpp
+++ Main.cpp
@@ -8,20 +8,14 @@
 #include <limits> 
 #include <string>
 
-int main() {
+int main() 
+{
 
-	
 	ProfessorOak professor("Professor Oak");
 	Player player;
-
-	
 	professor.greetPlayer(player);
 	professor.offerPokemonChoices(player);
-
-	
 	professor.explainMainQuest(player);
-
-	
 	Game game;
 	game.gameLoop(player);
 
